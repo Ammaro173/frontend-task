2c1aa96c02b1258a20fe86167727f6f75646d6c0
Ammar Abul-Feilat <ammaraf97@gmail.com>
2022-07-27 20:22:24 +0300

    fixed wrong push

--- pages/components/Search.jsx
+++ pages/components/Search.jsx
@@ -1,25 +1,5 @@
-import React, { useState, useEffect } from 'react';
-
 
 const SearchBar = ({ click, change, resetClick, options_applications, options_actions }) => {
-
-	// let options_applications = [null, 'CERT_TITLE_DEED_PLOT', 'LEASE_REGISTRATION', 'ADD_POA', 'ADD_COMPANY', 'ADD_COMPANY_EMPLOYEE', 'CERT_PROP_OWNERSHIP', 'LEASE_CLOSURE']
-	// let options_actions = [null, 'DARI_REFRESH_TOKEN', 'DARI_APP_LOGIN', 'INITIATE_APPLICATION', 'SUBMIT_APPLICATION', 'ADD_EMPLOYEE']
-
-	// useEffect(() => {
-	// 	let options_applications = JSON.parse(localStorage.getItem('options_applications'))
-	// 	console.log("???", options_applications?.length)
-	// 	let options_actions = JSON.parse(localStorage.getItem('options_actions'))
-	// 	function showOptions(options) {
-	// 		return options?.map((item, index) => {
-	// 			console.log('--->>', item);
-	// 			return <option key={index} value={item}>{item}</option>
-	// 		})
-	// 	}
-	// 	showOptions(options_applications);
-	// }, []);
-
-
 
 
 	return (
@@ -31,11 +11,10 @@ const SearchBar = ({ click, change, resetClick, options_applications, options_ac
 				<p>Action Type
 				</p>
 				<select className='filter' name='action_type' type='text' onChange={change} aria-label='Selector' >
+					<option value='' disabled selected hidden></option>
 					{
-						options_actions?.map((option) => (
-							<option key={option} value={option}>
-								{option}
-							</option>))
+						options_actions?.map((option, index) => (
+							<option key={index} value={option}>{option}</option>))
 					}
 
 
@@ -44,11 +23,13 @@ const SearchBar = ({ click, change, resetClick, options_applications, options_ac
 			<div className="search-input">
 				<p>Application Type</p>
 				<select className='filter' name='application_type' type='text' onChange={change} aria-label='Selector' >
+					<option value='' disabled selected hidden></option>
 					{
-						options_applications?.map((option) => (
-							<option key={option} value={option}>
-								{option}
-							</option>))
+						options_applications?.map((option, index) => (
+							option ?
+								<option key={index} value={option}>
+									{option}
+								</option> : null))
 					}
 				</select >
 			</div>
